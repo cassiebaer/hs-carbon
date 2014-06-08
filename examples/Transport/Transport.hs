4e3b5cddc3cb7766367f6449309b5321c5c389a9
Casper M. H. Holmgreen <cholmgreen@gmail.com>
2014-06-08 23:01:14 +0200

    Add comments to Transport example files

--- examples/Transport/Transport.hs
+++ examples/Transport/Transport.hs
@@ -40,6 +40,7 @@ type Angle = (Float,Float)
 
 type Simulation = ReaderT (Float -> (Float,Float)) (StateT ParticleState (MonteCarlo TFGen))
 
+-- Helper function for getting the cross-section data for the current energy
 getMu :: Simulation (Float,Float)
 getMu = do
     en <- gets remEnergy
@@ -47,12 +48,14 @@ getMu = do
     return (rho*t,rho*a)
   where rho = 1 -- g/cm^3 (water)
 
+-- Helper functions for sampling random numbers
 uniform :: Simulation Float
 uniform = lift (lift mcUniform)
-
 uniformR :: (Float,Float) -> Simulation Float
 uniformR bounds = lift (lift (mcUniformR bounds))
 
+-- Flies the particle some random distance with prob. according to
+--  cross-section data
 fly :: Simulation ()
 fly = do
     (PS i en (x,y) (ux,uy) ps) <- get
@@ -61,26 +64,31 @@ fly = do
     let s = -(log eta / mu_t)
     put (PS i en (x+ux*s,y+uy*s) (ux,uy) ps)
 
+-- The main loop responsible for a single photon's lifetime
 loop :: Simulation [(Point,Energy)]
 loop = do
     untilM_ (fly >> scatter) isBelowCutoff
     exit
 
+-- Terminates a particle if its energy is below the cutoff
 isBelowCutoff :: Simulation Bool
 isBelowCutoff = do
     en <- gets remEnergy
     return $ en < 10
 
+-- Returns the path stored in the ParticleState
 exit :: Simulation [(Point,Energy)]
 exit = do
     ps <- gets path
     return $ ps
 
+-- Randomly determines whether the scattering event scatters left or right
 _scatterDir :: Simulation Float
 _scatterDir = do
     eta <- uniform
     return $ if eta >= 0.5 then 1 else (-1)
 
+-- Scattering event; compute scattering angle, record collision site
 scatter :: Simulation ()
 scatter = do
     (PS i en (x,y) (ux,uy) ps) <- get
@@ -91,12 +99,11 @@ scatter = do
     let (ux',uy') = rotate (ux,uy) angle
     put (PS (i+1) (en-deltaW) (x,y) (ux',uy') (ps++[((x,y),deltaW)]))
 
-waveLength :: Energy -> Float
-waveLength en = 1240 / (en * 1000) -- en given in keV
-
+-- Computes the angle to rotate based on energy exchanged in coll.
 diffAngle :: Energy -> Energy -> Float
 diffAngle en en' = acos $ 1 - 0.511 * (1/en' - 1/en) -- Knuth
 
+-- Rotates a vector
 rotate :: Point -> Float -> Point
 rotate (x,y) th = (x*cos th - y*sin th, x*sin th + y*cos th)
 
@@ -117,8 +124,8 @@ main = do
     evaluate (rnf bs)
     let lengthF = fromIntegral . length
     let avgCol = (foldl' (+) 0 (map lengthF bs)) / lengthF bs :: Double
-    print avgCol
-    --displayResults bs
+    putStrLn $ "Average number of collisions: " ++ show avgCol
+    displayResults bs
 
 displayResults :: [[(Point, Energy)]] -> IO ()
 displayResults res = display (InWindow "Sim." (800,800) (200,200))
